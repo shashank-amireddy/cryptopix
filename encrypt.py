3054b27cdf153937aafefd8b9b34c0d459a9d3ab
shashank-amireddy <bl.en.u4cse22102@bl.students.amrita.edu>
2024-03-19 19:26:31 +0530

    created encrypt file with all functions

--- encrypt.py
+++ encrypt.py
@@ -2,38 +2,34 @@ from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad, unpad
 from stegano import lsb
-from PIL import Image
 
-def encrypt_text(text, key):
+def encrypt_text(text,key):
     cipher = AES.new(key, AES.MODE_ECB)
-    ct_bytes = cipher.encrypt(pad(text.encode(), AES.block_size))
+    ct_bytes = cipher.encrypt(pad(text.encode(),AES.block_size))
     return ct_bytes
 
 def decrypt_text(ciphertext, key):
     cipher = AES.new(key, AES.MODE_ECB)
-    plaintext = unpad(cipher.decrypt(ciphertext), AES.block_size)
+    plaintext = unpad(cipher.decrypt(ciphertext),AES.block_size)
     return plaintext.decode()
 
 def hide_text_in_image(text, input_image_path, output_image_path):
     encrypted_text = encrypt_text(text, key)
     secret = lsb.hide(input_image_path, encrypted_text.hex())
     secret.save(output_image_path)
-
+    
 def extract_text_from_image(image_path, key):
     extracted_text_hex = lsb.reveal(image_path)
     ciphertext = bytes.fromhex(extracted_text_hex)
-    decrypted_text = decrypt_text(ciphertext, key)  
+    decrypted_text = decrypt_text(ciphertext, key)
     return decrypted_text
 
-# Example usage
 input_image_path = "quantoomloom_2.png"
 output_image_path = "stego_image.png"
-key = get_random_bytes(16)  
-text_to_hide = "This "
+key = get_random_bytes(16)
+text_to_hide = "THIS IS A SECRET MESSAGE"
 
-
-hide_text_in_image(text_to_hide, input_image_path, output_image_path)
-
+hide_text_in_image(text_to_hide,input_image_path,output_image_path)
 
 extracted_text = extract_text_from_image(output_image_path, key)
 print("Extracted text:", extracted_text)
